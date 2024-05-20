097c48f5e12049cb9e90d68e148e5ec333e22a0b
Serhiy Chekh <123201474+xmottion@users.noreply.github.com>
2024-05-20 14:41:19 +0300

    Delete SDVM directory

--- SDVM/reg/animation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    anime.timeline({loop: false})
-      .add({
-        targets: 'body',
-        opacity: [0, 1],
-        easing: 'easeInOutQuad',
-        duration: 1500,
-      });
-  });
